6697ce96e81bc8fc44d91e5af7f71d96182397fd
lukasz <geblukasz@gmail.com>
2018-05-20 20:22:35 +0200

    calculator implementation

--- src/main/java/com/translationcalculator/translationform/controller/SimpleWebController.java
+++ src/main/java/com/translationcalculator/translationform/controller/SimpleWebController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class SimpleWebController {
@@ -21,9 +23,12 @@ public class SimpleWebController {
         model.addAttribute("freelancerCalculator", new FreelancerCalculator());
         return "freelancerform";
     }
- 
-    @RequestMapping(value="/freelancerform", method=RequestMethod.POST)
-    public String customerSubmit(@ModelAttribute FreelancerCalculator freelancerCalculator, Model model, ModelMap map) {
+
+    @RequestMapping(value = "/freelancerform", method = RequestMethod.POST)
+    String customerSubmit(@ModelAttribute FreelancerCalculator freelancerCalculator, Model model, ModelMap map, HttpServletRequest request) {
+        freelancerCalculator.setTranslationType(request.getParameter("translationType"));
+        freelancerCalculator.setVatPayer(request.getParameter("isVatPayer"));
+
         model.addAttribute("freelancerCalculator", freelancerCalculator);
         map.put("totalPrice", freelancerCalculator.calculate());
 
