83fe7cf7e4c59dfa360d203f3e674e81d042d4cb
lukasz <geblukasz@gmail.com>
2018-05-21 23:35:59 +0200

    templates fix

---
+++ src/main/java/com/translationcalculator/translationform/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.translationcalculator.translationform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping(value = "/")
+    public String welcome(){
+        return "index";
+    }
+}
