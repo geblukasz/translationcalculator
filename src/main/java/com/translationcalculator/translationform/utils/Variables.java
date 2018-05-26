e3c1b90ce60cb612b5e4474b2820a239cacc9f75
lukasz <geblukasz@gmail.com>
2018-05-26 08:38:23 +0200

    utils refactor

--- src/main/java/com/translationcalculator/translationform/utils/Variables.java
+++ src/main/java/com/translationcalculator/translationform/utils/Variables.java
@@ -3,10 +3,10 @@ package com.translationcalculator.translationform.utils;
 public class Variables {
 
     /* NO TAX, NO CAT */
-        //STAKE
+    //STAKE
     public static final double STAKE_PL_EN_NO_TAX_NO_CAT = 30.00;
     public static final double STAKE_EN_PL_NO_TAX_NO_CAT = 25.00;
-        // PAGE VOLUME
+    // PAGE VOLUME
     public static final int PAGE_VOLUME_PL_EN_NO_TAX_NO_CAT = 1800;
     public static final int PAGE_VOLUME_EN_PL_NO_TAX_NO_CAT = 1800;
 
@@ -14,16 +14,16 @@ public class Variables {
 
 
     /* WITH TAX, NO CAT */
-        //STAKE
+    //STAKE
     public static final double STAKE_PL_EN_WITH_TAX_NO_CAT = 30.00;
     public static final double STAKE_EN_PL_WITH_TAX_NO_CAT = 25.00;
-        // PAGE VOLUME
+    // PAGE VOLUME
     public static final int PAGE_VOLUME_PL_EN_WITH_TAX_NO_CAT = 1800;
     public static final int PAGE_VOLUME_EN_PL_WITH_TAX_NO_CAT = 1800;
 
 
     /* NO TAX, WITH CAT */
-        //ONE WORD PRICE
+    //ONE WORD PRICE
     public static final double ONE_WORD_PRICE_PL_EN_NO_TAX_WITH_CAT = 0.12;
     public static final double ONE_WORD_PRICE_EN_PL_NO_TAX_WITH_CAT = 0.10;
 
